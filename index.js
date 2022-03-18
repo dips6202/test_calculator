a8195022b72a64d73408bc458612fa47eef68c17
dips6202 <deepikakumari6202@gmail.com>
2022-03-18 23:25:45 +0530

    initial commit

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ let screen= document.getElementById('screen');
 buttons = document.querySelectorAll('button');
 let screenValue = ''; 
 
+
 for(item of buttons){
-    item.addEventListener('click',(e)=>{
+     item.addEventListener('click',(e)=>{
         buttonText = e.target.innerText;
         console.log('Button text is ', buttonText);
         if(buttonText=='x'){
