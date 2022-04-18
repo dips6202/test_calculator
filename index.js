28685307ceb92f53b446f3712b8fdc5443e4ad5b
dips6202 <deepikakumari6202@gmail.com>
2022-04-18 23:16:26 +0530

    initial commit

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ for(item of buttons)
 {
      item.addEventListener('click',(e)=>{ 
 
-        buttonText = e.target.innerText;
-        
+        buttonText = e.target.innerText;        
         console.log('Button text is ', buttonText); 
-
         if(buttonText=='x'){
             buttonText = '*';
             screenValue += buttonText;
