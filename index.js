23a41da2c42fbbf3a48ce80658c4ad2e8e4d8b61
dips6202 <deepikakumari6202@gmail.com>
2022-05-05 21:02:44 +0530

    initial commit

--- index.js
+++ index.js
@@ -6,19 +6,18 @@ for(item of buttons)
 {     item.addEventListener('click',(e)=>{ 
         buttonText = e.target.innerText; 
         
-        console.log('Button text is ', buttonText); 
         
+        console.log('Button text is ', buttonText); 
         if(buttonText=='x'){
             buttonText = '*';
             screenValue += buttonText;            
             screen.value += screenValue;
         }
-                
          else if (buttonText =='AC'){
-
             screen.value += "=";
             screen.value += " ";
        }
+       
         else if (buttonText =='='){
             screen.value = eval(screenValue);
         } 
