0c07762c82f7af8fd08d9bd1c81c04d80a8c74ee
dips6202 <deepikakumari6202@gmail.com>
2022-05-12 23:10:19 +0530

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ buttons = document.querySelectorAll('button');
 let screenValue = ''; 
 
 for(item of buttons)
-{     item.addEventListener('click',(e)=>{ 
+{     
+    item.addEventListener('click',(e)=>{ 
         buttonText = e.target.innerText; 
         
         
