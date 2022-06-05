42d6186999a86113f4c62630cad5ae8037dddedc
dips6202 <deepikakumari6202@gmail.com>
2022-06-05 20:16:13 +0530

    changes in function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ buttons = document.querySelectorAll('button');
 let screenValue = ''; 
 
 for(item of buttons)
-{     
+{    
     item.addEventListener('click',(e)=>{ 
         buttonText = e.target.innerText;      
         console.log('Button text is ', buttonText); 
